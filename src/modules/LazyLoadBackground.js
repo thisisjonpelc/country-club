1ea41b47bcc0743959569b091ea2381976c77c0e
Jon Pelc <thisisjonpelc@gmail.com>
2019-03-17 18:48:47 -0500

    Fixed some lazyload background bugs

--- src/modules/LazyLoadBackground.js
+++ src/modules/LazyLoadBackground.js
@@ -7,8 +7,12 @@ export default class LazyLoadBackground{
     events(){
         document.addEventListener('lazybeforeunveil', function(e){
             var bg = e.target.getAttribute('data-bg');
-                
+
+            console.log(e.target);
+            //console.log(bg);
+
             if(bg){
+                console.log('There is a background:', bg);
                 e.target.style.backgroundImage = `url(${bg})`;
             }
 
